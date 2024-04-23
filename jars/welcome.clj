c5df5a1a609d5a6508073b2ada5d7ac791f7bd6b
IvanovAnton4682671 <anton-ivanov-080203@mail.ru>
2024-04-23 12:18:48 +0300

    Add welcome-file

---
+++ jars/welcome.clj
@@ -0,0 +1,6 @@
+
+(defn welcomeInGame []
+    (println "Hi!")
+    (println "Welcome to Shakals Game!")
+    (println "Moving: FORWARD - w, LEFT - a, BACKWARD - s, RIGHT - d.")
+)
--- src/server.clj
+++ src/server.clj
@@ -2,12 +2,14 @@
 
 (ns server ;простарнство имён server будет использовать пространства имён clojure.contrib.server-socket и clojure.contrib.duck-streams
     (:use [clojure.contrib server-socket duck-streams]) ;server-socket работает с сокетами, а duck-streams работает с потоками ввода-вывода
+    (:require [welcome :refer [welcomeInGame]]) ;импортируем функцию-приветствие
 )
 
 (def port (* 5 1111)) ;будем подключаться к серверу по порту 5555 
 
 (defn server_base_fun [input_stream output_stream] ;основная функция, которая будет отвечать за ввод/вывод
     (binding [*in* (reader input_stream) *out* (writer output_stream)] ;биндим потоки ввода/вывода
+        (welcomeInGame) ;вызываем функцию-приветствие
         (loop [] ;запускаем бесконечный цикл
             (println (read-line)) ;печатает полученную строку от пользователя
             (recur) ;рекурсивный вызов себя же
