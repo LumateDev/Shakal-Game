738147043aae5ed3a74b196eb6ceb77d97d0f1ac
Lumate <sanchous557@mail.ru>
2024-05-26 20:32:39 +0300

    change wait methods

--- src/server.clj
+++ src/server.clj
@@ -10,10 +10,10 @@
 
 ;здесь объявляются все константы мультиплеера
 (def connections (atom []))
-(def port 5555) 
-(def max-players 2)
+(def port 5555)  ; будем подключаться к серверу по порту 5555
+(def max-players 2) ; максимальное колличество игроков
 (def players (atom [])) 
-(def current-turn (atom nil))
+(def current-turn (atom nil)) ; текущий код
 (def turn-queue (atom []))
 
 
@@ -69,7 +69,7 @@
 ;добавляем плеера к вектору всех игроков
 (defn add-player-to-players [player]
   (swap! players conj player) ; 1. Добавление нового игрока в список игроков.
-  (println "Current players:" @players) ; 2. Вывод текущего списка игроков в консоль.
+  ;(println-win (str "Current players: " @players)) ; 2. Вывод текущего списка игроков в консоль.
 )
 
 ;ищем игрока по имени
@@ -106,12 +106,19 @@
 
 )
 
+(defn update-thread [] ; тормозок
+    (read-line)
+    (println-win "wait...")
+)
+
 ;ожидает подключения всех игроков
 (defn wait-for-players []
   (while (< (count @players) max-players)
-    (println (str "Waiting for players...  " max-players))
-    (Thread/sleep 1000))
-  (println "All players are ready! The game is starting..."))
+    (println-win (str "Waiting for players...  " max-players))
+    (update-thread) ; Вызываем тормозок (так как у меня не работаю потоки то вот таой вариант, если работают потоки, коммитте это и откоммитте следующую строку)
+    ; (Thread/sleep 1000)
+    )
+  (println-win "All players are ready! The game is starting..."))
 
 
 ; ищет игрока по имени и обновляет его данные
@@ -133,6 +140,8 @@
                          (fn [player]
                            (assoc player :player-x new-x :player-y new-y))))
 
+ 
+
 
 (defn game-loop [game-map player output-stream]
   
@@ -148,12 +157,14 @@
          player-balance player-balance
          name name]
     
-    (print "NICK: ")
-    (print @turn-queue)
+    (println-win (str "NICK: " name)) ; Имя пользхователя текущего потока
 
-    (if (current-player-turn? name)(do ;если ход текущего игрока
+    (println-win (str "Current queue: " @turn-queue)) ; Очередь ходов
+
+
+    
     (update-player-coordinates players name player-x player-y)  ;обновляем координаты игрока со значениями, которые пришли на прошлой итерации
-    (print @players) 
+    ; (print @players) 
     (println-win "\u001b[32;1mYour stats:\u001b[0m")
     (print-lives player-lives)
     (print-armor player-armor)
@@ -166,7 +177,7 @@
     (print "\n")
     (print-map-with-explored game-map explored)
     (flush)
-    
+    (if (current-player-turn? name)(do ;если ход текущего игрока
     (let [input (read-line)]
       (cond
         (= input "exit")
@@ -182,11 +193,13 @@
           (if (= new-map game-map)
             (do
               (next-player) ; передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map) ; КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name))
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur new-map new-explored player-x (dec player-y) player-lives new-armor new-damage new-balance name))))
 
         (= input "a")
@@ -195,13 +208,15 @@
           (if (= new-map game-map)
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
                (update-player-coordinates players name (- player-x 1) (+ player-y 0))
                               
               (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name))
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur new-map new-explored (dec player-x) player-y player-lives new-armor new-damage new-balance name))))
 
         (= input "s")
@@ -210,11 +225,13 @@
           (if (= new-map game-map)
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name))
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur new-map new-explored player-x (inc player-y) player-lives new-armor new-damage new-balance name))))
 
         (= input "d")
@@ -223,11 +240,13 @@
           (if (= new-map game-map)
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name))
             (do
               (next-player); передает ход
-              (recur-and-print-map new-map)
+              (recur-and-print-map new-map); КАЖДЫЙ РАЗ  ПЕРЕД РЕКУРСИВНЫМ ИЗМЕНЕНИЕМ КАРТЫ ДЛЯ ПОЛЬЗОВАТЕЛЯ 
+              ; ПЕРЕДАЁМ ТЕКУЩЕЕ СОСТОЯНИЕ КАРТЫ В ФУНКЦИЮ ЗАПИСИ В ФАЙЛ
               (recur new-map new-explored (inc player-x) player-y player-lives new-armor new-damage new-balance name))))
 
         :else
@@ -238,23 +257,24 @@
           (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name)))))
  (do ;если не ход текущего игрока
         
-        (println "Not your turn")
-        (Thread/sleep 1000)
+        (println-win "Not your turn!")
+        (update-thread) ; Вызываем тормозок (так как у меня не работаю потоки то вот таой вариант, если работают потоки, коммитте это и откоммитте следующую строку)
+        ; (Thread/sleep 1000)
         (recur game-map explored player-x player-y player-lives player-armor player-damage player-balance name))))))
 ;повторение всей красоты
 
 (def game-map
-  (let [initial-game-map (create-empty-map)
-        game-map-with-treasures (place-treasures initial-game-map treasure-count treasure-symbol)
+  (let [initial-game-map (create-empty-map) ;создание пустой карты
+        game-map-with-treasures (place-treasures initial-game-map treasure-count treasure-symbol) ;разместить сокровища на карте
 
 
-        game-map-with-common-weapons (place-treasures game-map-with-treasures common-weapons-count common-weapons-symbol)
-        game-map-with-rare-weapons (place-treasures game-map-with-common-weapons rare-weapons-count rare-weapons-symbol)
-        game-map-with-legendary-weapons (place-treasures game-map-with-rare-weapons legendary-weapons-count legendary-weapons-symbol)
+        game-map-with-common-weapons (place-treasures game-map-with-treasures common-weapons-count common-weapons-symbol) ; разместить обычные оружия на карте
+        game-map-with-rare-weapons (place-treasures game-map-with-common-weapons rare-weapons-count rare-weapons-symbol) ; разместить редкие оружия на карте
+        game-map-with-legendary-weapons (place-treasures game-map-with-rare-weapons legendary-weapons-count legendary-weapons-symbol) ; разместить легендарные оружия на карте
 
-        game-map-with-common-armor (place-treasures game-map-with-legendary-weapons common-armor-count common-armor-symbol)
-        game-map-with-rare-armor (place-treasures game-map-with-common-armor rare-armor-count rare-armor-symbol)
-        game-map-with-legendary-armor (place-treasures game-map-with-rare-armor legendary-armor-count legendary-armor-symbol)
+        game-map-with-common-armor (place-treasures game-map-with-legendary-weapons common-armor-count common-armor-symbol) ; разместить обычныую броню на карте
+        game-map-with-rare-armor (place-treasures game-map-with-common-armor rare-armor-count rare-armor-symbol) ; разместить редкую броню на карте
+        game-map-with-legendary-armor (place-treasures game-map-with-rare-armor legendary-armor-count legendary-armor-symbol) ; разместить легендарную броню на карте  
 
         game-map-with-enemy (place-players game-map-with-legendary-armor @players enemies-symbol) ;должна создавать игроков, но не работает
     ]
